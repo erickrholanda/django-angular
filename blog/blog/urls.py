c8b150fcdc00a3ae834154de99cbbd818c663c11
Erick <erick.holanda@sirius.com.br>
2020-10-18 00:34:08 -0300

    Ajustes

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from rest_framework import routers
 from django.urls import include, path
 from core import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r'members', views.MemberViewSet)
@@ -27,4 +29,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/core/views.py
+++ blog/core/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 from .models import Member
 from .serializers import MemberSerializer, MemberSimpleSerializer
+
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication 
+
+# class CsrfExemptSessionAuthentication(SessionAuthentication):
+
+#     def enforce_csrf(self, request):
+#         return  # To not perform the csrf check previously happening
 
 class MemberViewSet(viewsets.ModelViewSet):
     """
@@ -12,6 +18,7 @@ class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
     # permission_classes = [permissions.IsAuthenticated]
+    # authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
 
     def list(self, request, *args, **kwargs):
         queryset = Member.objects.all()
